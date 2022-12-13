470697234f865b21f2871723a7f19539ac638c39
Meka-oss <340878842@gapps.yrdsb.ca>
2022-12-13 13:28:52 -0500

    Final Edits

--- Titlescreen.java
+++ Titlescreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Titlescreen extends World
 {
     Label titleLabel = new Label("Kanye \u262E West\n Simulator", 100);
-    
+    Label titleLabelb = new Label("press g to start", 50);
     /**
      * Constructor for objects of class Titlescreen.
      * 
@@ -20,11 +20,12 @@ public class Titlescreen extends World
         super(600, 400, 1); 
         
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabelb, 300, 350);
     }
     
     public void act()
     {
-        if(Greenfoot.isKeyDown("space"))
+        if(Greenfoot.isKeyDown("g"))
         {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
